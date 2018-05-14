bb9c1cf6c7772034e9e21c9f86ccf74d82a08d08
Trylobot <trylobot@gmail.com>
2018-05-14 14:12:37 -0500

    use-latest as optional param

--- curseforge-manifest-downloader.js
+++ curseforge-manifest-downloader.js
@@ -2,12 +2,19 @@ const path = require('path')
 const fs = require('fs')
 const request = require('request')
 
+if (!process.argv[2]) {
+  console.log('usage: <path/to/manifest.json> [--latest]')
+  process.exit()
+}
+
 const manifest_path = process.argv[2]
+const use_latest = process.argv[3] == '--latest'
 const manifest_json = JSON.parse(fs.readFileSync(manifest_path))
 const dest = `${path.dirname(manifest_path)}/mods/`
 manifest_json.files.forEach( function( file ) {
-  const url = `http://minecraft.curseforge.com/projects/${file.projectID}/files/latest`
-  // const url = `http://minecraft.curseforge.com/projects/${file.projectID}/files/${file.fileID}/download`
+  const url = use_latest ? 
+    `http://minecraft.curseforge.com/projects/${file.projectID}/files/latest` :
+    `http://minecraft.curseforge.com/projects/${file.projectID}/files/${file.fileID}/download`
   request({ url, encoding:null }, function( error, response, body ) {
     if (!error && response.statusCode == 200 && body) {
       const filename = decodeURIComponent(path.basename(response.request.href))
